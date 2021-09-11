b56b81525c301964285717bf6fb3c7d8aa6582dd
gabyiy <gabyiy2000@hotmail.com>
2021-09-11 08:32:25 +0000

    modal

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -3,8 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import Popper from "popper.js";
+import $ from "jquery";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
