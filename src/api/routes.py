a593c3648a0756f6248789e03a713e2e68646616
gabyiy <gabyiy2000@hotmail.com>
2021-09-17 15:40:31 +0000

    next

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Product
 from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import jwt_required, get_jwt_identity, create_access_token, JWTManager
 from flask_mail import Message
@@ -90,3 +90,9 @@ def fileUpload():
 
     return jsonify(result)
 
+@api.route('/products', methods=["GET"])
+def getProducts():
+
+    products = Product.getAllProducts()
+
+    return jsonify({"data": products})
