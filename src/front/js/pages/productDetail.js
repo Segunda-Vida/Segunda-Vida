2b1ea90c8e35cb5a36c5e57615f56e980947164e
Krlos160287 <martigracia2018@gmail.com>
2021-09-26 12:53:25 +0000

    avanzando

--- src/front/js/pages/productDetail.js
+++ src/front/js/pages/productDetail.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
@@ -14,29 +14,55 @@ export const ProductDetail = () => {
 	return (
 		<div>
 			<Navbar />
-			<div className="card-deck">
-				<div className="card">
-					<img
-						src={store.product.product_image_url}
-						className="card-img-top"
-						style={{ width: "100px", height: "160px" }}
-					/>
-					<div className="card-body">
-						<h1 className="card-title">Nombre: {store.product.name}</h1>
-						<p className="card-text">
-							<h3>Marca</h3>
-							<h4>{store.product.brand}</h4>
-							<h3>Description</h3>
-							<h4>{store.product.description}</h4>
-							<h3>Precio</h3>
-							<h4>{store.product.price}</h4>
-						</p>
-						<p className="card-text">
-							<small className="text-muted">Last updated 3 mins ago</small>
-						</p>
+
+			{store.list.map(item => {
+				let images = JSON.parse(item.product_image_url);
+				console.log(images);
+				return (
+					<div className="container" key={item.id} style={{ margin: "50px" }}>
+						<div className="card-deck, row">
+							<div className="card, col-6">
+								{images[0] ? (
+									<img
+										src={images[0]}
+										className="card-img-top"
+										style={{ width: "100px", height: "160px", margin: "30px" }}
+									/>
+								) : null}
+								{images[1] ? (
+									<img
+										src={images[1]}
+										className="card-img-top"
+										style={{ width: "100px", height: "160px" }}
+									/>
+								) : null}
+								{images[2] ? (
+									<img
+										src={images[2]}
+										className="card-img-top"
+										style={{ width: "100px", height: "160px" }}
+									/>
+								) : null}
+
+								<div className="card-body">
+									<h1 className="card-title">Nombre: {item.name}</h1>
+									<p className="card-text">
+										<h3>Marca</h3>
+										<h4>{item.brand}</h4>
+										<h3>Description</h3>
+										<h4>{item.description}</h4>
+										<h3>Precio</h3>
+										<h4>{item.price}</h4>
+									</p>
+									<p className="card-text">
+										<small className="text-muted">Last updated 3 mins ago</small>
+									</p>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				);
+			})}
 		</div>
 	);
 };
