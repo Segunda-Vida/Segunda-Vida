200bd0c7d84ddd19e0382aee2c84be74be86c703
Krlos160287 <martigracia2018@gmail.com>
2021-09-26 18:03:24 +0000

    Casi

--- src/front/js/pages/productDetail.js
+++ src/front/js/pages/productDetail.js
@@ -15,59 +15,38 @@ export const ProductDetail = () => {
 		<div>
 			<Navbar />
 
-			{store.list.map(item => {
-				let images = JSON.parse(item.product_image_url);
-				let userId = JSON.parse(item.user_id);
-				console.log(images);
-				return (
-					<div key={item.id}>
-						{!userId ? (
-							<div className="container" style={{ margin: "50px" }}>
-								<div className="card-deck, row">
-									<div className="card, col-6">
-										{images[0] ? (
-											<img
-												src={images[0]}
-												className="card-img-top"
-												style={{ width: "100px", height: "160px", margin: "30px" }}
-											/>
-										) : null}
-										{images[1] ? (
-											<img
-												src={images[1]}
-												className="card-img-top"
-												style={{ width: "100px", height: "160px" }}
-											/>
-										) : null}
-										{images[2] ? (
-											<img
-												src={images[2]}
-												className="card-img-top"
-												style={{ width: "100px", height: "160px" }}
-											/>
-										) : null}
-
-										<div className="card-body">
-											<h1 className="card-title">Nombre: {item.name}</h1>
-											<p className="card-text">
-												<h3>Marca</h3>
-												<h4>{item.brand}</h4>
-												<h3>Description</h3>
-												<h4>{item.description}</h4>
-												<h3>Precio</h3>
-												<h4>{item.price}</h4>
-											</p>
-											<p className="card-text">
-												<small className="text-muted">Last updated 3 mins ago</small>
-											</p>
-										</div>
+			<div>
+				<div className="container" style={{ margin: "50px" }}>
+					<div className="card-deck, row">
+						<div className="card, col-6">
+							{store.list.map(item => {
+								let images = JSON.parse(item.product_image_url);
+								console.log(images);
+								return (
+									<div key={item}>
+										<img src={images[0]} className="card-img-top" alt="..." />
 									</div>
-								</div>
-							</div>
-						) : null}
+								);
+							})}
+						</div>
+
+						<div className="card-body">
+							<h1 className="card-title">Nombre: {store.product.name}</h1>
+							<p className="card-text">
+								<h3>Marca</h3>
+								<h4>{store.product.brand}</h4>
+								<h3>Description</h3>
+								<h4>{store.product.description}</h4>
+								<h3>Precio</h3>
+								<h4>{store.product.price}</h4>
+							</p>
+							<p className="card-text">
+								<small className="text-muted">Last updated 3 mins ago</small>
+							</p>
+						</div>
 					</div>
-				);
-			})}
+				</div>
+			</div>
 		</div>
 	);
 };
