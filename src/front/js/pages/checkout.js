a04588dc68a33d8220c8903196cffbe9448f26dd
Krlos160287 <martigracia2018@gmail.com>
2021-09-28 19:30:26 +0000

    Import navbar in checkout

--- src/front/js/pages/checkout.js
+++ src/front/js/pages/checkout.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Navbar } from "../component/navbar.js";
 import { StripeButton } from "../component/StripeButton.js";
 
 export const CheckoutPage = () => {
@@ -24,32 +25,35 @@ export const CheckoutPage = () => {
 	const total = cart.reduce((acc, cur) => acc + cur.qty * cur.price, 0);
 
 	return (
-		<div className="checkout-page">
-			<div className="head-block">
-				<span className="description">Description</span>
-				<span className="quantity">Quantity</span>
-				<span className="remove">Remove</span>
-				<span className="Price">Price</span>
+		<div>
+			<Navbar />
+			<div className="checkout-page">
+				<div className="head-block">
+					<span className="description">Description</span>
+					<span className="quantity">Quantity</span>
+					<span className="remove">Remove</span>
+					<span className="Price">Price</span>
+				</div>
+				<div className="content-block">
+					{cart.map(item => (
+						<div className="item" key={item.id}>
+							<div className="description">
+								<img src={""} alt="" />
+								<h4>{item.name}</h4>
+							</div>
+							<div className="quantity">
+								<span>{item.qty}</span>
+							</div>
+							<div className="remove" onClick={() => removeItem(item)}>
+								X
+							</div>
+							<div className="price">$ {item.price}</div>
+						</div>
+					))}
+				</div>
+				<div className="total">Total: ${total}</div>
+				<StripeButton totalAmount={total} />
 			</div>
-			<div className="content-block">
-				{cart.map(item => (
-					<div className="item" key={item.id}>
-						<div className="description">
-							<img src={""} alt="" />
-							<h4>{item.name}</h4>
-						</div>
-						<div className="quantity">
-							<span>{item.qty}</span>
-						</div>
-						<div className="remove" onClick={() => removeItem(item)}>
-							X
-						</div>
-						<div className="price">$ {item.price}</div>
-					</div>
-				))}
-			</div>
-			<div className="total">Total: ${total}</div>
-			<StripeButton totalAmount={total} />
 		</div>
 	);
 };
