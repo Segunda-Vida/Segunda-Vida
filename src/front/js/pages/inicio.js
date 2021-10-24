d802a55c403c83981699ca0d417bc2313e03d8db
Krlos160287 <martigracia2018@gmail.com>
2021-10-24 21:16:08 +0000

    Loading complete, falta cambiar rutas y prueba

--- src/front/js/pages/inicio.js
+++ src/front/js/pages/inicio.js
@@ -10,37 +10,35 @@ export const Inicio = () => {
 
 	return (
 		<div className="container text-center">
-			<div
-				style={{
-					display: "inline-list-item",
-					justifyContent: "center",
-					alignItems: "center",
-					marginTop: "15%",
-					marginBottom: "20%",
-					border: "1px solid rgb(96,96,258)",
-					borderRadius: "25px",
-					backgroundImage:
-						" url(https://www.wallpapertip.com/wmimgs/3-34573_blue-wallpaper-hd-blue-and-white-hd.jpg)"
-				}}>
-				<h1 id="Titulo">SEGUNDA VIDA</h1>
-				<h2> Bienvenidos a nuestra tienda online de productos de segunda mano.</h2>
-				<h2>Aqui vais a encontrar productos de buena calidad y vendedores de confianza.</h2>
-				<h2>Sin gastos de portes en el transporte de envios y de rapida entrega al cliente.</h2>
-				<h2>Metodos de pago seguros.</h2>
-				<h2>En caso de que el producto no sea de su agrado, es posible la devolución sin cargo</h2>
-				<Link to="/">
-					<button
-						className="btn btn-outline-primary my-2 my-sm0"
-						type="submit"
-						href="#"
-						style={{ marginRight: "5px" }}>
-						<i className="fas fa-door-open">Regresar</i>
-					</button>
-				</Link>
+			<div>
+				<img
+					src="https://ci6.googleusercontent.com/proxy/f58kg40ctZ-DjhiD6L9M9vEpMSPGkplGEqNHtYRsfVq_6h6l-DjWCLl8TnIbby73psRpjEHcNTu6_70ATCRiqT3BPAqvUv5aJEYg4-sFQ1Tcu_3lpaTb_pMAMmci9apZMibcvAXe5fZ3t2Wk4_RaEFg0eQ=s0-d-e1-ft#https://api.freelogodesign.org/files/287dfdcf9dcb476399e52fabe7cd0308/thumb/logo_200x200.png?v=0"
+					style={{ width: "200px", height: "200px" }}
+				/>
 				<div className="text-center">
 					<div className="spinner-border text-primary" role="status">
 						<span className="sr-only">Loading...</span>
 					</div>
+				</div>
+				<div
+					style={{
+						border: "1px solid rgb(96,96,248)",
+						borderRadius: "25px",
+						backgroundImage:
+							"url(https://www.wallpapertip.com/wmimgs/3-34573_blue-wallpaper-hd-blue-and-white-hd.jpg)"
+					}}>
+					<h2 id="Titulo"> CARGANDO PAGINA</h2>
+
+					<h2 style={{ color: "rgb(96, 96, 248)" }}>
+						Bienvenidos a nuestra tienda online de productos de segunda mano.
+					</h2>
+					<h2 style={{ color: "rgb(96, 96, 248)" }}>
+						Vais a encontrar productos de buena calidad y vendedores de confianza.
+					</h2>
+					<h2 style={{ color: "rgb(96, 96, 248)" }}>Metodos de pago seguros.</h2>
+					<h2 style={{ color: "rgb(96, 96, 248)" }}>
+						En caso de que el producto no sea de su agrado, es posible la devolución sin cargo
+					</h2>
 				</div>
 			</div>
 		</div>
